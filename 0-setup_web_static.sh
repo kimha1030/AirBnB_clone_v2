dfc4bdf9238e6a65f9371788a027a67bf32a92c0
kimha1030 <1478@holbertonschool.com>
2020-08-17 23:23:49 -0500

    update Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,6 @@ sudo mkdir -p "/data/web_static/releases/test/"
 sudo mkdir -p "/data/web_static/shared/"
 echo -e "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "51i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
