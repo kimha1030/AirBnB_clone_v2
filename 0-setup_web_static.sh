18579ba3f84d17953ce535feeb834b9e44aa16aa
kimha1030 <1478@holbertonschool.com>
2020-08-17 20:03:46 -0500

    0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Task 0
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p "/data/web_static/shared/"
+sudo mkdir -p "/data/web_static/releases/test/"
+echo -e "<html>\n<head></head>\n<body>Holberton School</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown ubuntu:ubuntu /data/
+sudo sed -i "51i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo service nginx restart
