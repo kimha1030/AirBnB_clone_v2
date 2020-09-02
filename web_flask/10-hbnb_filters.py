5e0556c3e8bffe5ef719f300a2e6a1212a4a7ce9
kimha1030 <1478@holbertonschool.com>
2020-09-02 17:26:46 +0000

    Add task 10

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Task 6"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """Function that return states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def list_states(id):
+    """Function that return states according id"""
+    states = storage.all(State).values()
+    ban = False
+    name_st = ""
+    for state in states:
+        if id in state.id:
+            ban = True
+            name_st = state
+            break
+        else:
+            ban = False
+    return render_template('9-states.html', name_st=name_st, ban=ban)
+
+
+@app.teardown_appcontext
+def close_session(db):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
